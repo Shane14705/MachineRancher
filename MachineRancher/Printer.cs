df59466f4d20843ed4ede6e9e02a0974d471e090
Shane McKelvey <psmnhb@umsystem.edu>
2024-05-15 23:38:03 -0500

    Successfully got mqtt to property linking working for limited types. Anything more complicated than that requires parsing on the machine's end.

--- MachineRancher/Printer.cs
+++ MachineRancher/Printer.cs
@@ -31,9 +31,24 @@ namespace MachineRancher
         private float fan_speed;
 
         [MonitorRegistration("Printers/*/moonraker/status/connections", "websocket")]
-        public (string, int) Websocket { get => websocket; set => websocket = value; }
+        public string Websocket
+        {
+            get
+            {
+                
+                return (websocket_addr + websocket_port.ToString());
+            }
+            set
+            {
+                var temp = value.Split(":");
+                websocket_addr = temp[0];
+                int.TryParse(temp[1], out websocket_port);
+            }
+        }
 
-        private (string, int) websocket;
+        private string websocket_addr;
+
+        private int websocket_port;
 
         //[MonitorRegistration("Printers/*/moonraker/state/nozzle_size")]
         //public float Nozzle_Size { get => nozzle_size; set => nozzle_size = value; } //Note: we can do averaging here, or do it in the mqtt monitor. Probably more efficient to do in the monitor
